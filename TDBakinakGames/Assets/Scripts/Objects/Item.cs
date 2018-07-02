e4ef6b8e87a321059a466d52b1c535ef1ecec829
Unknown <lqwn16@student.aau.dk>
2018-07-02 17:02:32 +0200

    destructible Vase

---
+++ TDBakinakGames/Assets/Scripts/Objects/Item.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour {
+
+	public GameObject destroydVersion;
+
+	 void OnCollisionEnter(Collision other)
+	{
+		// You probably want a check here to make sure you're hitting a zombie
+		// Note that this is not the best method for doing so.
+		if ( other.gameObject.CompareTag("player")){
+			Instantiate (destroydVersion, transform.position, transform.rotation);
+			this.gameObject.SetActive (false);
+			Debug.Log ("Collide");
+
+		}
+	}
+}
