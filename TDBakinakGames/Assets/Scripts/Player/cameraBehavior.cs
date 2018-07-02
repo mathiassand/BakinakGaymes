57910b30c6728a3c5590f3b2b59d2b300436f847
Unknown <akobbe16@student.aau.dk>
2018-07-02 14:03:11 +0200

    Basic camera and player movement
    Open the scene called Anders Test to see it.

---
+++ TDBakinakGames/Assets/Scripts/Player/cameraBehavior.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraBehavior : MonoBehaviour {
+
+	public Transform player;
+	public float smooth = 0.1f;
+	public float zPosition = 1;
+	public float yPosition = 15;
+
+	private Vector3 velocity = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 position = new Vector3 ();
+		if (GameObject.FindGameObjectWithTag ("Player") != null) {
+			position.x = player.position.x;
+			position.z = player.position.z - zPosition;
+			position.y = player.position.y + yPosition;
+
+			transform.position = Vector3.SmoothDamp (transform.position, position, ref velocity, smooth);
+		}
+
+	}
+
+	//Methods
+}
