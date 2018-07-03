7af55cec03668eeac896c3b17ea55017b74874b7
Unknown <akobbe16@student.aau.dk>
2018-07-03 13:36:22 +0200

    More camera stuff
    Camera now changes position and angle when you press "v", keeping the player in the center of the screen. Movement still fucked though.

--- TDBakinakGames/Assets/Scripts/Player/cameraBehavior.cs
+++ TDBakinakGames/Assets/Scripts/Player/cameraBehavior.cs
@@ -8,26 +8,106 @@ public class cameraBehavior : MonoBehaviour {
 	public float smooth = 0.1f;
 	public float zPosition = 1;
 	public float yPosition = 15;
+	public float cameraAngle = 70;
+	int angle = 0;
+	int anglePos = 0;
 
 	private Vector3 velocity = Vector3.zero;
+	private Vector3 position = new Vector3 ();
 
 	// Use this for initialization
 	void Start () {
-
+		transform.eulerAngles = new Vector3 (cameraAngle, 0, 0);
 	}
 
 	// Update is called once per frame
 	void Update () {
-		Vector3 position = new Vector3 ();
 		if (GameObject.FindGameObjectWithTag ("Player") != null) {
-			position.x = player.position.x;
-			position.z = player.position.z - zPosition;
-			position.y = player.position.y + yPosition;
+			switch (anglePos) {
+			case 0:
+				camPosition0 ();
+				break;
 
-			transform.position = Vector3.SmoothDamp (transform.position, position, ref velocity, smooth);
+			case 1:
+				camPosition1 ();
+				break;
+
+			case 2:
+				camPosition2 ();
+				break;
+
+			case 3:
+				camPosition3 ();
+				break;
+
+			}
+		}
+
+		if(Input.GetKeyUp("v")){
+			changeAngle ();
 		}
 
 	}
 
 	//Methods
+	void changeAngle (){
+		angle += 1;
+		if (angle > 3) {
+			angle = 0;
+		}
+
+		anglePos += 1;
+		if (anglePos > 3) {
+			anglePos = 0;
+		}
+
+		switch (angle) {
+		case 0:
+			transform.eulerAngles = new Vector3 (cameraAngle, 0, 0);
+			break;
+
+		case 1:
+			transform.eulerAngles = new Vector3 (cameraAngle, 90, 0);
+			break;
+
+		case 2:
+			transform.eulerAngles = new Vector3 (cameraAngle, 180, 0);
+			break;
+
+		case 3:
+			transform.eulerAngles = new Vector3 (cameraAngle, 270, 0);
+			break;
+
+		}
+			
+	}
+
+	void camPosition0 (){
+		position.x = player.position.x;
+		position.z = player.position.z - zPosition;
+		position.y = player.position.y + yPosition;
+		transform.position = Vector3.SmoothDamp (transform.position, position, ref velocity, smooth);
+	}
+
+	void camPosition1 (){
+		position.x = player.position.x - zPosition;
+		position.z = player.position.z;
+		position.y = player.position.y + yPosition;
+		transform.position = Vector3.SmoothDamp (transform.position, position, ref velocity, smooth);
+	}
+
+	void camPosition2 (){
+		position.x = player.position.x;
+		position.z = player.position.z + zPosition;
+		position.y = player.position.y + yPosition;
+		transform.position = Vector3.SmoothDamp (transform.position, position, ref velocity, smooth);
+	}
+		
+	void camPosition3 (){
+		position.x = player.position.x + zPosition;
+		position.z = player.position.z;
+		position.y = player.position.y + yPosition;
+		transform.position = Vector3.SmoothDamp (transform.position, position, ref velocity, smooth);
+	}
+
 }
